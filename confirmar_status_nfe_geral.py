a9c98687c5465168e8585f4302714232764f7647
guilhermeG23 <guilhermebrechot@gmail.com>
2021-02-05 10:39:22 -0300

    Atualizado o servico para atender a nova demanda

--- confirmar_status_nfe_geral.py
+++ confirmar_status_nfe_geral.py
@@ -1,42 +1,69 @@
+#Libs necessarias
 import requests
 from bs4 import BeautifulSoup
 
 #Capturando todos os status de todos os serviços
 link = "https://www.nfe.fazenda.gov.br/portal/disponibilidade.aspx?versao=0.00&tipoConteudo=Skeuqr8PQBY="
-site_receita = requests.get(link).text
-soup = BeautifulSoup(site_receita, 'html.parser')
-todas_tr_impar = soup.findAll("tr", {"class":"linhaImparCentralizada"})
-todas_tr_par = soup.findAll("tr", {"class":"linhaParCentralizada"})
-todas_tr = todas_tr_impar + todas_tr_par
+site_receita = requests.get(link).text #Requests to text.html
+soup = BeautifulSoup(site_receita, 'html.parser') #Lendo o conteudo do requests
+todos_titulos = soup.findAll("th") #Todos os titulos
+todas_tr_impar = soup.findAll("tr", {"class":"linhaImparCentralizada"}) #Todas linhas impars TR
+todas_tr_par = soup.findAll("tr", {"class":"linhaParCentralizada"}) #Todas linhas pares TR
+todas_tr = todas_tr_impar + todas_tr_par #Juntando as TR
 
-#Limpeza
+#Limpeza -> Confere o status das imagens e textos
 def confirmar_status(entrada):
-    entrada = entrada['src']
-    if entrada == "imagens/bola_verde_P.png":
-        return True
+    #Confere se é imagema
+    try:
+        imagens = entrada.findAll("img")
+        imagens = imagens[0]['src']
+    except:
+        imagens = None
+    #Retorno dos valores
+    if imagens is not None:
+        #Se tem servico com status anormal
+        if imagens == "imagens/bola_verde_P.png":
+            return True
+        else:
+            return False
     else:
-        return False
+        #Texto
+        if len(entrada.text) == 0:
+            return "-"
+        else:
+            return entrada.text
+            
+#Titulos
+campos = []
+for i in todos_titulos:
+    campos.append(i.text)
 
-#Buscando  os campos 
-todas_tr_limpa = []
+#Buscando os campos 
+todos_campos = []
 for linha in todas_tr:
-    for campo in linha:
-        #O try é por causa do findall
-        try:
-            entrada = campo.findAll("img")
-            if (len(entrada)) == 1:
-                todas_tr_limpa.append(entrada[0])
-        except:
-            pass
+    estados = []
+    for i in zip(campos, linha.findAll("td")):
+        posLimpeza = [i[0], confirmar_status(i[1])]
+        estados.append(posLimpeza)
+    todos_campos.append(estados)
 
-#Acusa se tiver problemas
-problemas = False
-for valor in todas_tr_limpa:
-    if not(confirmar_status(valor)):
-        print("1")
-        problemas = True
-        break
+#Conferindo o que esta com problemas
 
-#Ultima msg caso nao tiver problemas
-if not(problemas):
-    print("0")
+#status inicial do problema
+problemas = False #Mensagem que deu ruim
+locais_com_problemas = ["Estado ==> Servico ==> Status"] #Guardar os status da operacao
+
+#Buscando quem ta com problemas
+for linhas_campos in todos_campos:
+    for c in linhas_campos:
+        #So mostrar que ta com problema
+        if c[1] == False:
+            locais_com_problemas.append("{} ==> {} ==> Problema".format(linhas_campos[0][1],c[0]))
+            problemas = True
+
+#Saida dos status
+if problemas:
+    for i in locais_com_problemas:
+        print(i)
+else:
+    print("Status Nfe Ok")           
